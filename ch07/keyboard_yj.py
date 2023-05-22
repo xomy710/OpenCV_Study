31eaaa8457a401e44ef2e2da555ddcec9c1c9b59
xomy <xomy710@gmail.com>
2023-05-22 20:44:04 +0900

    7-6

---
+++ ch07/keyboard_yj.py
@@ -0,0 +1,31 @@
+import sys
+import numpy as np
+import cv2
+
+src = cv2.imread('keyboard.bmp', cv2.IMREAD_GRAYSCALE)
+
+if src is None:
+    print('Image load failed!')
+    sys.exit()
+
+_, src_bin = cv2.threshold(src, 0, 255, cv2.THRESH_OTSU)
+
+cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(src_bin)
+
+dst = cv2.cvtColor(src, cv2.COLOR_GRAY2BGR)
+dst2 = cv2.morphologyEx(dst, cv2.MORPH_OPEN, None)
+
+
+for i in range(1, cnt):
+    (x, y, w, h, area) = stats[i]
+
+    if area < 20:
+        continue
+
+    cv2.rectangle(dst2, (x, y, w, h), (0, 255, 255), 4)
+
+cv2.imshow('src', src)
+cv2.imshow('src_bin', src_bin)
+cv2.imshow('dst2', dst2)
+cv2.waitKey()
+cv2.destroyAllWindows()
